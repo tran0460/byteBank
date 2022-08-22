7ca3ac079d8e57f840fd963a264c10cdae509c87
Hygor Costa <cost0142@algonquinlive.com>
2022-08-22 14:35:23 -0400

    add account Structure

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # byteBank
+fun main() {
+println("Bem vindo ao Bytebank")
+var i = 0
+while (i < 5) {
+val titular: String = "Hygor $i"
+val numeroConta: Int = 1000 + i
+var saldo = 10.0 + i
+
+
+
+        println("Titular -->$titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+
+//    for (i in 5 downTo 1) {
+//        val titular: String = "Hygor $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 10.0 + i
+//
+//
+//
+//        println("Titular -->$titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+
+//    testaCondicoes(saldo)
+}
+
+
+fun testaCondicoes(saldo: Double) {
+//    two ways to write tha same if/else
+//    if (saldo > 0.0){
+//        println("conta é positiva")
+//    } else if (saldo == 0.0) {
+//        println("conta é neutra")
+//    } else {
+//        println("Conta negativa")
+//    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("Conta negativa")
+    }
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,73 @@
-fun main(){
-println("Bem vindo ao Bytebank")
-    val titular: String = "Hygor"
-    val numeroConta: Int = 1000
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+
+    val contaHygor = Conta()
+    contaHygor.titular = "Hygor"
+    contaHygor.numero = 1000
+    contaHygor.saldo = 200.0
+
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1002
+    contaFran.saldo = 300.0
+
+
+    println(contaHygor.titular)
+    println(contaHygor.numero)
+    println(contaHygor.saldo)
+    println()
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
     var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += + 350
-    saldo -= 1000
+}
+
+fun testaLacos() {
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Hygor $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = 10.0 + i
 
 
-    println("Titular -->$titular")
-    println("numero da conta $numeroConta")
-    println("saldo da conta $saldo")
 
-//    two ways to write tha same if/else
+        println("Titular -->$titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+
+//    for (i in 5 downTo 1) {
+//        val titular: String = "Hygor $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 10.0 + i
+//
+//
+//
+//        println("Titular -->$titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+
+//    testaCondicoes(saldo)
+}
+
+
+fun testaCondicoes(saldo: Double) {
+    //    two ways to write tha same if/else
     //    if (saldo > 0.0){
 //        println("conta é positiva")
 //    } else if (saldo == 0.0) {
@@ -26,6 +81,5 @@ println("Bem vindo ao Bytebank")
         saldo == 0.0 -> println("conta é neutra")
         else -> println("Conta negativa")
     }
-
 
 }
